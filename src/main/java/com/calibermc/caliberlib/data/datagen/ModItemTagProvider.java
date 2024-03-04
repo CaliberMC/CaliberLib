f1dab6411c1268656c000a91e85ddb1c6656d232
CaliberMC <j.luttmer@calibertech.co>
2024-03-04 00:26:28 -0800

    Added Tag Data Gen. Removed duplicate wood pressure plates from BlockManager variants.

---
+++ src/main/java/com/calibermc/caliberlib/data/datagen/ModItemTagProvider.java
@@ -0,0 +1,173 @@
+package com.calibermc.caliberlib.data.datagen;
+
+import com.calibermc.caliberlib.block.management.BlockManager;
+import com.calibermc.caliberlib.data.ModBlockFamily;
+import com.mojang.datafixers.util.Pair;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.data.PackOutput;
+import net.minecraft.data.tags.ItemTagsProvider;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.ItemTags;
+import net.minecraft.tags.ItemTags;
+import net.minecraft.tags.ItemTags;
+import net.minecraft.world.item.*;
+import net.minecraft.world.level.block.*;
+import net.minecraft.world.level.block.state.properties.BlockSetType;
+import net.minecraftforge.common.Tags;
+import net.minecraftforge.common.data.ExistingFileHelper;
+import net.minecraftforge.registries.ForgeRegistries;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Map;
+import java.util.Objects;
+import java.util.concurrent.CompletableFuture;
+import java.util.function.Supplier;
+
+public class ModItemTagProvider extends ItemTagsProvider {
+
+    protected final String modid;
+
+    public ModItemTagProvider(PackOutput pOutput, CompletableFuture<HolderLookup.Provider> pLookupProvider,
+                              CompletableFuture<TagLookup<Block>> pItemTags, String modid, @Nullable ExistingFileHelper existingFileHelper) {
+        super(pOutput, pLookupProvider, pItemTags, modid, existingFileHelper);
+        this.modid = modid;
+    }
+
+    @Override
+    protected void addTags(HolderLookup.Provider pProvider) {
+
+        for (BlockManager blockManager : BlockManager.BLOCK_MANAGERS.get(this.modid)) {
+            for (Map.Entry<BlockManager.BlockAdditional, Pair<ResourceLocation, Supplier<Block>>> e : blockManager.getBlocks().entrySet()) {
+                ModBlockFamily.Variant variant = e.getKey().variant;
+                String blockName = e.getValue().getFirst().getPath();
+                BlockSetType blockSetType = blockManager.blockType();
+
+                if (blockName.contains("crimson") || blockName.contains("warped")) {
+                    this.tag(ItemTags.NON_FLAMMABLE_WOOD).add(e.getValue().getSecond().get().asItem());
+                }
+
+                if (blockName.contains("thatch")) {
+                    this.tag(ItemTags.DAMPENS_VIBRATIONS).add(e.getValue().getSecond().get().asItem());
+                }
+
+                if (variant.equals(ModBlockFamily.Variant.BASE)) {
+                    if (blockName.contains("cobbled")) {
+                        this.tag(ItemTags.STONE_CRAFTING_MATERIALS).add(e.getValue().getSecond().get().asItem());
+                    } else if (blockName.contains("limestone_bricks")) {
+                        this.tag(ItemTags.STONE_BRICKS).add(e.getValue().getSecond().get().asItem());
+                    } else if (blockName.contains("log") || (blockName.contains("wood"))) {
+                        this.tag(ItemTags.LOGS_THAT_BURN).add(e.getValue().getSecond().get().asItem());
+                    } else if (blockName.contains("planks")) {
+                        this.tag(ItemTags.PLANKS).add(e.getValue().getSecond().get().asItem());
+                    }
+                }
+
+                if (variant.equals(ModBlockFamily.Variant.DOOR)) {
+                    this.tag(ItemTags.WOODEN_DOORS).add(e.getValue().getSecond().get().asItem());
+                }
+
+                if (variant.equals(ModBlockFamily.Variant.FENCE)) {
+                    this.tag(ItemTags.WOODEN_FENCES).add(e.getValue().getSecond().get().asItem());
+                }
+
+                if (variant.equals(ModBlockFamily.Variant.FENCE_GATE)) {
+                    this.tag(ItemTags.WOODEN_FENCES).add(e.getValue().getSecond().get().asItem());
+                }
+
+                if (variant.equals(ModBlockFamily.Variant.CEILING_HANGING_SIGN)) {
+                    this.tag(ItemTags.HANGING_SIGNS).add(e.getValue().getSecond().get().asItem());
+                }
+
+                if (variant.equals(ModBlockFamily.Variant.PRESSURE_PLATE)) {
+                    this.tag(ItemTags.WOODEN_PRESSURE_PLATES);
+                }
+
+                if (variant.equals(ModBlockFamily.Variant.SIGN)) {
+                    this.tag(ItemTags.SIGNS).add(e.getValue().getSecond().get().asItem());
+                }
+
+                if (variant.equals(ModBlockFamily.Variant.SLAB)) {
+                    this.tag(ItemTags.SLABS).add(e.getValue().getSecond().get().asItem());
+                }
+
+                if (variant.equals(ModBlockFamily.Variant.STAIRS)) {
+                    this.tag(ItemTags.STAIRS).add(e.getValue().getSecond().get().asItem());
+                }
+
+                if (variant.equals(ModBlockFamily.Variant.TRAPDOOR)) {
+                    this.tag(ItemTags.TRAPDOORS).add(e.getValue().getSecond().get().asItem());
+                }
+            }
+        }
+
+        ForgeRegistries.BLOCKS.getValues().stream()
+                .filter(block -> {
+                    ResourceLocation registryName = ForgeRegistries.BLOCKS.getKey(block);
+                    return BlockManager.ALL_BLOCKS.stream().map(Supplier::get).noneMatch(b -> b.equals(block))
+                            && registryName != null && registryName.getNamespace().equals(this.modId);
+                })
+                .forEach(block -> {
+                    String itemName = Objects.requireNonNull(ForgeRegistries.BLOCKS.getKey(block)).getPath();
+
+                    if (itemName.contains("crimson") || itemName.contains("warped")) {
+                        this.tag(ItemTags.NON_FLAMMABLE_WOOD).add(block.asItem());
+                    }
+
+                    if (itemName.contains("leaves")) {
+                        this.tag(ItemTags.LEAVES).add(block.asItem());
+                    }
+
+                    if (itemName.contains("sand") && !itemName.contains("stone")) {
+                        this.tag(ItemTags.SAND).add(block.asItem());
+                        this.tag(ItemTags.SMELTS_TO_GLASS).add(block.asItem());
+                        this.tag(Tags.Items.SAND).add(block.asItem());
+                    }
+
+                    if (itemName.contains("ingot")) {
+                        this.tag(ItemTags.TRIM_MATERIALS).add(block.asItem());
+                    }
+
+                    if (itemName.contains("ore")) {
+                        this.tag(Tags.Items.ORES).add(block.asItem());
+                        if (itemName.contains("deepslate")) {
+                            this.tag(Tags.Items.ORES_IN_GROUND_DEEPSLATE).add(block.asItem());
+                        } else if (itemName.contains("nether")) {
+                            this.tag(Tags.Items.ORES_IN_GROUND_NETHERRACK).add(block.asItem());
+                        } else {
+                            this.tag(Tags.Items.ORES_IN_GROUND_STONE).add(block.asItem());
+                        }
+                    }
+                });
+
+        // Item Tags
+        for (Map.Entry<ResourceKey<Item>, Item> resourceKeyItemEntry : ForgeRegistries.ITEMS.getEntries()) {
+            // Check if the item's registry name is in the MOD_ID namespace
+            ResourceLocation registryName = resourceKeyItemEntry.getKey().location();
+            String itemName = registryKey.toString();
+            if (registryName != null && this.modId.equals(registryName.getNamespace())) {
+                Item item = resourceKeyItemEntry.getValue();
+                Block block = Block.byItem(item);
+
+                if (item instanceof SwordItem) {
+                    this.tag(ItemTags.SWORDS).add(item);
+                }
+
+                if (item instanceof PickaxeItem) {
+                    this.tag(ItemTags.PICKAXES).add(item);
+                }
+
+                if (item instanceof ShovelItem) {
+                    this.tag(ItemTags.SHOVELS).add(item);
+                }
+
+                if (item instanceof HoeItem) {
+                    this.tag(ItemTags.HOES).add(item);
+
+                }
+            }
+        }
+
+    }
+}
+
---
+++ src/main/java/com/calibermc/caliberlib/util/ModTags.java
@@ -0,0 +1,75 @@
+package com.calibermc.caliberlib.util;
+
+import com.calibermc.caliberlib.CaliberLib;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.BlockTags;
+import net.minecraft.tags.ItemTags;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+
+public class ModTags {
+
+    public static class Blocks {
+
+        /* Shapes */
+        public static final TagKey<Block> ARCHES = tag("shapes/arches");
+        public static final TagKey<Block> ARROWSLITS = tag("shapes/arrowslits");
+        public static final TagKey<Block> BALUSTRADES = tag("shapes/balustrades");
+        public static final TagKey<Block> HORIZONTAL_BEAMS = tag("shapes/horizontal_beams");
+        public static final TagKey<Block> BEAM_LINTELS = tag("shapes/beam_lintels");
+        public static final TagKey<Block> BEAM_POSTS = tag("shapes/beam_posts");
+        public static final TagKey<Block> VERTICAL_BEAMS = tag("shapes/vertical_beams");
+        public static final TagKey<Block> CAPTIALS = tag("shapes/capitals");
+        public static final TagKey<Block> COLUMNS = tag("shapes/columns");
+        public static final TagKey<Block> CORNERS = tag("shapes/corners");
+        public static final TagKey<Block> DOOR_FRAMES = tag("shapes/door_frames");
+        public static final TagKey<Block> DOOR_FRAME_LINTELS = tag("shapes/door_frame_lintels");
+        public static final TagKey<Block> EIGHTHS = tag("shapes/eighths");
+        public static final TagKey<Block> HALF_ARCHES = tag("shapes/half_arches");
+        public static final TagKey<Block> HALF_LARGE_ARCHES= tag("shapes/half_large_arches");
+        public static final TagKey<Block> HALF_WINDOWS = tag("shapes/half_windows");
+        public static final TagKey<Block> LARGE_ARCHES = tag("shapes/large_arches");
+        public static final TagKey<Block> LAYERS = tag("shapes/layers");
+        public static final TagKey<Block> PILLARS = tag("shapes/pillars");
+        public static final TagKey<Block> QUARTERS = tag("shapes/quarters");
+        public static final TagKey<Block> ROOF_22S = tag("shapes/roof_22s");
+        public static final TagKey<Block> ROOF_45S = tag("shapes/roof_45s");
+        public static final TagKey<Block> ROOF_67S = tag("shapes/roof_67s");
+        public static final TagKey<Block> ROOF_PEAKS = tag("shapes/roof_peaks");
+        public static final TagKey<Block> TALL_WOODEN_DOORS = tag("shapes/tall_wooden_doors");
+        public static final TagKey<Block> VERTICAL_CORNER_SLABS = tag("shapes/vertical_corner_slabs");
+        public static final TagKey<Block> VERTICAL_LAYERS = tag("shapes/vertical_layers");
+        public static final TagKey<Block> VERTICAL_QUARTERS = tag("shapes/vertical_quarters");
+        public static final TagKey<Block> VERTICAL_SLABS = tag("shapes/vertical_slabs");
+        public static final TagKey<Block> WINDOWS = tag("shapes/windows");
+
+
+        /* Forge Block Tags */
+        public static final TagKey<Block> GRANITE = forgeTag("granite");
+        public static final TagKey<Block> MARBLE = forgeTag("marble");
+
+        private static TagKey<Block> tag(String name) {
+            return BlockTags.create(new ResourceLocation("caliber", name));
+        }
+        private static TagKey<Block> forgeTag(String name) {
+            return BlockTags.create(new ResourceLocation("forge", name));
+        }
+
+    }
+
+    public static class Items {
+        /* Forge Item Tags */
+//        public static final TagKey<Item> BRONZE_INGOTS = forgeTag("ingots/bronze_ingot");
+
+        private static TagKey<Item> tag(String name) {
+            return ItemTags.create(new ResourceLocation("caliber", name));
+        }
+
+        private static TagKey<Item> forgeTag(String name) {
+            return ItemTags.create(new ResourceLocation("forge", name));
+        }
+    }
+
+}
+
